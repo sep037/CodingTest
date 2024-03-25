4f9b699afd2e9dc18446a721cdfe716eb68065ac
바ㄱ스느 <112964257+sep037@users.noreply.github.com>
2024-03-26 01:54:07 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -2,5 +2,5 @@ def solution(my_string, m, c):
     answer = ''
     num_rows = len(my_string) // m
     for i in range(num_rows):
-        answer += my_string[i * m + c - 1]  # 인덱스는 0부터 시작하므로 c에서 1을 빼줌
+        answer += my_string[i * m + c - 1]
     return answer
