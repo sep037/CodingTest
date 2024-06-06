237f93905a696fa417ed8b2b1bb9434cc990002c
승은 <112964257+sep037@users.noreply.github.com>
2024-06-06 19:23:29 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+print(fib(n))
